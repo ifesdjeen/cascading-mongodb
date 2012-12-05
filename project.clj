f22b5ed4c9f0f03aa2092aa462845a5b3e60c849
Paul Lam <paul@quantisan.com>
2012-12-05 10:40:54 +0000

    make use of :provided dependencies for hadoop and cascading

--- project.clj
+++ project.clj
@@ -2,12 +2,9 @@
   :description "Cascading MongoDB Tap"
   :url "http://github.com/ifesdjeen/cascading-mongodb"
   :min-lein-version "2.0.0"
-  :dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]
-                 [org.mongodb/mongo-hadoop-streaming "1.1.0-SNAPSHOT"]
+  :dependencies [[org.mongodb/mongo-hadoop-streaming "1.1.0-SNAPSHOT"]
                  ;; [mongo-hadoop-streaming_cdh3u3  "1.0.0-rc0"]
                  [org.mongodb/mongo-hadoop-core_cdh3u3 "1.0.0"]
-                 [cascading/cascading-core "2.0.2"]
-                 [cascading/cascading-hadoop "2.0.2"]
                  [log4j/log4j "1.2.17"]]
   :java-source-paths ["src/main/java"]
   :test-paths        ["src/main/test"]
@@ -16,7 +13,10 @@
                                   [com.novemberain/monger "1.2.0"]
                                   [cascalog "1.10.0"]
                                   [midje "1.3.0" :exclude [org.clojure/clojure]]
-                                  [midje-cascalog "0.4.0" :exclude [org.clojure/clojure]]]}}
+                                  [midje-cascalog "0.4.0" :exclude [org.clojure/clojure]]]}
+             :provided {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]
+                                       [cascading/cascading-core "2.0.2"]
+                                       [cascading/cascading-hadoop "2.0.2"]]}}
   :test-selectors {:all     (constantly true)
                    :focus   :focus
                    :default (constantly true)}
